72829ec045de0aa95e71937d2be34a1271214715
LeonardoSalmento <Salmento_@hotmail.com>
2020-10-14 20:26:21 -0300

    Adicionando validações

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,16 @@ import express from 'express';
 import './database/connection';
 import 'express-async-errors';
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 import errorHandler from './errors/handler';
 
 const app = express();
-
+ 
+app.use(cors());
 app.use(express.json());
-app.use(routes); 
+app.use(routes);
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
