588bbf3f5561fd08141f3a6e8924cd07ae2b307a
LeonardoSalmento <Salmento_@hotmail.com>
2020-10-17 23:01:15 -0300

    Adicionando páginas de detalhes e criação de orfanatos, e criação do componente header

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import mapMarker from '../images/map-marker.png';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export default function OrphanageMap(){
   const navigation = useNavigation();
 
   function handleNavigationToDetails(){
-    navigation.navigate('OrphanagesDetails');
+    navigation.navigate('OrphanageDetails');
+  }
+
+  function handleNavigationCreateOrphanage(){
+    navigation.navigate('SelectMapPosition');
   }
 
 
@@ -48,9 +53,9 @@ export default function OrphanageMap(){
       <View style={styles.footer}>
           <Text style={styles.footerText}>2 orfanatos encontrados</Text>
 
-          <TouchableOpacity style={styles.createOrphanageButton} onPress={() =>{}}>
+          <RectButton style={styles.createOrphanageButton} onPress={handleNavigationCreateOrphanage}>
           <Feather name="plus" size={20} color="#FFF" />
-          </TouchableOpacity>
+          </RectButton>
       </View>
     </View>
     )
